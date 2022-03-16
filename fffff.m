63b3790374eb31c80f2f481d9d9defd6dcd41598
Maddy <37761862+Maddy54652@users.noreply.github.com>
2022-03-15 23:31:34 -0700

    Create fffff.m

---
+++ fffff.m
@@ -0,0 +1,11 @@
+%2.1 algorithm for Math 448
+%created March 15, 2022 by Maddy Henderson
+
+function[p] = fffff(f,endpoint_a,endpoint_b,TOL,maxIter)
+    %inputs:
+    %f is the user input function
+    %endpoint_a-endpoint a
+    %endpoint_b-endpoint b
+    %TOL is the Tolerance
+    %maxIter-short for max iterations
+    %output it p; which is an approximate solution
